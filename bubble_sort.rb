9808aad11f14dd0509b450a08a41ecd9297fe266
Abdullah Numan <anewman15@hotmail.com>
2020-05-29 18:23:18 +0600

    Modify bubble_sort method to retain the original array and return a new one

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,20 +1,23 @@
 def bubble_sort(arr)
-  arr_size = arr.length
+  sorted_arr = arr[0..-1]
+  
+  arr_size = sorted_arr.length
   
   (arr_size - 1).times do
     i = 0
     while i < arr_size - 1
-      a = arr[i]
-      b = arr[i + 1]
-      if arr[i] > arr[i + 1]
-        arr[i] = b
-        arr[i + 1] = a
+      a = sorted_arr[i]
+      b = sorted_arr[i + 1]
+      if sorted_arr[i] > sorted_arr[i + 1]
+        sorted_arr[i] = b
+        sorted_arr[i + 1] = a
       end
       i += 1
     end
   end
-  arr
+  sorted_arr
 end
   
 arr = [12, 10, 9, 7, 6, 5, 4, 2]
 p bubble_sort(arr)
+p arr
