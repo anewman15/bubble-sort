dbf749096bc3fb5fc2e84ae6bded8114e9d85564
Abdullah Numan <anewman15@hotmail.com>
2020-06-01 14:13:33 +0600

    Combine both methods into bubble_sort.rb file, remove debugging code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -18,6 +18,26 @@ def bubble_sort(arr)
   sorted_arr
 end
 
-arr = [12, 10, 9, 7, 6, 5, 4, 2]
-p bubble_sort(arr)
-p arr
+def bubble_sort_by(arr)
+  sorted_arr = arr.clone
+  arr_size = arr.length
+
+  (arr_size - 1).times do
+    i = 0
+    while i < arr_size - 1
+      a = sorted_arr[i]
+      b = sorted_arr[i + 1]
+      if (yield sorted_arr[i], sorted_arr[i + 1]).positive?
+        sorted_arr[i] = b
+        sorted_arr[i + 1] = a
+      end
+      i += 1
+    end
+  end
+  sorted_arr
+end
+
+array_string = %w[]
+bubble_sort_by(array_string) do |left, right|
+  left.length - right.length
+end
